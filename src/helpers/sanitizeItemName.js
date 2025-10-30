837f652b4a6464a5aaddf27fd37dbf63a4c8452b
mrahm <martin.lars.rahm+msd@gmail.com>
2025-10-30 18:23:26 +0100

    Update sanitizer to account for uppercase

--- src/helpers/sanitizeItemName.js
+++ src/helpers/sanitizeItemName.js
@@ -1,13 +1,21 @@
 export const stringToLowerCase = (string) => string.toLowerCase();
+export const stringToUpperCase = (string) => string.toUpperCase();
 
 export function sanitizeItemName(brand, name) {
-  const combinedString = stringToLowerCase(name).includes(
-    stringToLowerCase(brand)
-  )
-    ? `${brand} ${stringToLowerCase(name)
-        .replace(stringToLowerCase(brand), ``)
-        .trim()}`
-    : `${brand} ${name}`;
+  const duplicateWords = name.split(/\s+/);
+
+  const filterWords = duplicateWords.filter(
+    (word) => stringToLowerCase(word) !== stringToLowerCase(brand)
+  );
+
+  let joinedWords = filterWords.join(" ").trim();
+
+  if (joinedWords.charAt(0) !== stringToUpperCase(joinedWords.charAt(0))) {
+    const firstCharacter = stringToUpperCase(joinedWords.charAt(0));
+    joinedWords = joinedWords.replace(joinedWords.charAt(0), firstCharacter);
+  }
+
+  const combinedString = `${brand} ${joinedWords}`;
 
   return combinedString;
 }
